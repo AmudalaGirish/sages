355eac9796d5cae1955c1df81297838aa22d6d6a
Girish Amudala <amudalagirish@gmail.com>
2023-05-31 19:26:16 +0530

    last changes today

--- solutions/admin.py
+++ solutions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import GasStorageSolution, GasPipeline, GasApplicationSolution, GasFuelConversion, ValueAddedService, GasProduct
 
-# Register your models here.
+admin.site.register(GasStorageSolution)
+admin.site.register(GasPipeline)
+admin.site.register(GasApplicationSolution)
+admin.site.register(GasFuelConversion)
+admin.site.register(ValueAddedService)
+admin.site.register(GasProduct)
--- solutions/models.py
+++ solutions/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class GasStorageSolution(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    # Add any additional fields specific to the Gas Storage Solution model
+
+    def __str__(self):
+        return self.title
+
+
+class GasPipeline(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    # Add any additional fields specific to the Gas Pipeline model
+
+    def __str__(self):
+        return self.title
+
+
+class GasApplicationSolution(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    # Add any additional fields specific to the Gas Application Solution model
+
+    def __str__(self):
+        return self.title
+
+
+class GasFuelConversion(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    # Add any additional fields specific to the Gas Fuel Conversion model
+
+    def __str__(self):
+        return self.title
+
+
+class ValueAddedService(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    # Add any additional fields specific to the Value Added Service model
+
+    def __str__(self):
+        return self.title
+
+
+class GasProduct(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    # Add any additional fields specific to the Gas Product model
+
+    def __str__(self):
+        return self.title
